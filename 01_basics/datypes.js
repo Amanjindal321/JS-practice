4e572d6a2706abab4ec649ea54ed3d91effc7f92
Aman Jindal <151001417+Amanjindal321@users.noreply.github.com>
2024-02-04 12:36:08 +0000

    datatypes

---
+++ 01_basics/datypes.js
@@ -0,0 +1,17 @@
+let name = "aman"
+let age = 22
+let isLogin = false
+
+// number =>  2 to power 53
+// bigint
+// string => ""
+// boolean
+// null
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "aman");
+console.log(typeof undefined);
+console.log(typeof null);
---
+++ 01_basics/varb.js
@@ -0,0 +1,7 @@
+const accountId = 1357
+let accountEmail = "amanjindal@google.com"
+var accountPassword = "12345"
+accountCity= "jaipur"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
